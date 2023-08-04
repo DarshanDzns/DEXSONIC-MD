bb9b5ac2eb740ea587b3fd0bcb516c83cae76551
DarshanDzns <140175381+DarshanDzns@users.noreply.github.com>
2023-08-04 17:14:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.email = 'divakarjainkar063@gmail.com'
+global.github = 'https://github.com/DarshanDzns'
+global.location = 'Pume IN'
+global.gurl = 'https://instagram.com/divakar_.fr' // add your username
+global.sudo = process.env.SUDO || '918767327657'
+global.devs = '919767327657';
+global.website = 'https://bento.me/darshandzns' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'SamPandey001' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '𝘿 𝙀 𝙓 𝘽𝙊𝙏' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𝙳 𝙸 𝚅 𝙰 𝙺 𝙰 𝚁🥷🏻' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
